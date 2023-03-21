b2852b75cd6d18d0216e0ab090e06b2bbdd99de8
Warren Zhang <warrenzhang17@gmail.com>
2023-03-21 16:25:16 -0400

    arrows pointed

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide() {
+    const divide = (parameter1, parameter2) => parameter1 / parameter2;
+    return divide(2000,100);
+}
+
+function square() {
+    const square = (parameter) => parameter * parameter;
+    return square(2);
+}
+
+function add() {
+    const add = (parameter1, parameter2) => parameter1 + parameter2;
+    return add(3,4);
+}
